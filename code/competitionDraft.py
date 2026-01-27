da0313e35eadf650165d1bbfb90ec4bb09ef5b1b
Techies Robotics <67329395+techiesrobotics@users.noreply.github.com>
2026-01-27 14:51:12 -0500

    Refactor drone control functions and paths

--- code/competitionDraft.py
+++ code/competitionDraft.py
@@ -1,41 +1,86 @@
 from codrone_edu.drone import *
 drone = Drone()
-drone.connect()
-
-
+drone.pair()
+# 1 - Take off
 drone.takeoff()
 
+def detectColorandSetLed(duration):        
+    drone.load_color_data("color_data")
 
-drone.get_pressure()
+    start_time = time.time()
 
+    while (time.time() - start_time) < duration:
 
-drone.set_throttle(67)
-drone.move(1.3)
-drone.set_throttle(20)
-#Approaching the ring
-drone.avoid_wall(99,20)
+        color_data = drone.get_color_data()
+        color = drone.predict_colors(color_data)
+        if color[0] == "green":
+            drone.set_drone_LED(0, 255, 0, 100)
+        elif color[0] == "blue":
+            drone.set_drone_LED(0, 0, 255, 100)
+        elif color[0] == "yellow":
+            drone.set_drone_LED(255, 255, 0, 100)
+        elif color[0] == "red":
+            drone.set_drone_LED(255, 0, 0, 100)
+        else:
+            drone.set_drone_LED(0, 0, 0, 0)
 
+        print(color[0])
+        time.sleep(0.2)
+        
+def changeHeight(power, time, hoverTime):
+    drone.set_throttle(power)
+    drone.move(time)
+    drone.set_throttle(20)
+    drone.hover(hoverTime)
 
-drone.set_throttle(80)
-drone.move(2)
-drone.set_throttle(20)
+def moveForward(power, time, hoverTime):
+    drone.set_pitch(power)  
+    drone.move(time)
+    drone.set_pitch(0)
+    drone.hover(hoverTime)
+    
+    #drone.move_forward(distance=50, units="cm", speed=1)
+    #drone.move_distance(0.5, 0.5, 0.25, 1) # move forward 0.5m, left 0.5m, and upward 0.25m simultaneously at 1m/s
 
+def moveRight(power, time, hoverTime):
+    drone.set_roll(power)  
+    drone.move(time)
+    drone.set_roll(0)
+    drone.hover(hoverTime)
 
-drone.hover(3)
+def moveLeft(power, time, hoverTime):
+    drone.set_roll(-1*power)  
+    drone.move(time)
+    drone.set_roll(0)
+    drone.hover(hoverTime)
 
+def path1():
+    changeHeight(67, 1.3, 2)
+    # pass gate and green keyhole
+    moveForward(60, 1, 1)
+    drone.land()
+    detectColorandSetLed(1)
 
-drone.set_pitch(60)
-drone.move(1.5)
-drone.set_pitch(0)
-#Descending from the ring
-drone.set_throttle(-60)
-drone.move(1.5)
-drone.set_throttle(10)
+def path2():
+    # 2 - Raise get ready for passing Red Gate
+    changeHeight(67, 1.3, 2)
+    # pass gate, green keyhole and panel
+    moveForward(80, 1, 1)
+    drone.land()
+    detectColorandSetLed(1)
 
+def path3():
 
-drone.set_pitch(40)
-drone.move(1)
-drone.set_pitch(0)
+    changeHeight(40, 1, 2)
+    moveForward(30,1,1)
+    changeHeight(-25, 1 ,2)
+    moveRight(30, 1,1)
+    moveForward(30,1,1)
+    moveLeft(30, 1,1)
+    moveForward(30,1,1)
+    drone.land()
+    drone.close()
 
-
-drone.land()
+#path1()
+#path2()
+path3()
